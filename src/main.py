95d7a18b1cffcfd9824ea61116c809ef4abc76e6
adldotori <adldotori@gmail.com>
2022-03-06 07:43:57 +0900

    feat: develop 5 apis

---
+++ src/main.py
@@ -0,0 +1,84 @@
+import os
+import json
+from dotenv import load_dotenv
+
+from flask import Flask, request, jsonify
+from flask_mongoengine import MongoEngine
+
+load_dotenv()  # take environment variables from .env.
+
+app = Flask(__name__)
+app.config["MONGODB_SETTINGS"] = {
+    "db": os.getenv("MONGODB_NAME"),
+    "host": "localhost",
+    "port": 27017,
+}
+db = MongoEngine()
+db.init_app(app)
+
+
+class User(db.Document):
+    id = db.IntField(primary_key=True)
+    name = db.StringField(unique=True)
+    age = db.IntField(null=True)
+
+    def to_json(self):
+        return {"id": self.id, "name": self.name, "age": self.age}
+
+
+@app.route("/user", methods=["GET"])
+def get_all_users():
+    user = User.objects.all()
+    return jsonify([i.to_json() for i in user]), 200
+
+
+@app.route("/user/<id>", methods=["GET"])
+def get_user(id):
+    user = User.objects(id=id).first()
+    if not user:
+        return jsonify({"error": "data not found"}), 404
+    else:
+        return jsonify(user.to_json()), 200
+
+
+@app.route("/user", methods=["POST"])
+def create_user():
+    record = json.loads(request.data)
+    if len(User.objects.all()) > 0:
+        id = User.objects.all().order_by("-id").first()["id"] + 1
+    else:
+        id = 1
+    user = User(id=id, name=record["name"], age=record["age"])
+    try:
+        user.save()
+    except:
+        return jsonify({"success": False}), 400
+    return jsonify({"success": True}), 201
+
+
+@app.route("/user/<id>", methods=["PUT"])
+def update_user(id):
+    record = json.loads(request.data)
+    user = User.objects(id=id).first()
+    if not user:
+        return jsonify({"success": False}), 400
+    else:
+        user.update(name=record["name"])
+        if "age" in record.keys():
+            user.update(age=record["age"])
+        user.save()
+        return jsonify({"success": True}), 201
+
+
+@app.route("/user/<id>", methods=["DELETE"])
+def delete_user(id):
+    user = User.objects(id=id).first()
+    if not user:
+        return jsonify({"success": False}), 400
+    else:
+        user.delete()
+        return jsonify({"success": True}), 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
